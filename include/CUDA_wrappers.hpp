6c70804b5addaab2b5d281017cf622f618b44ba5
Mr-Geekman <36005824+Mr-Geekman@users.noreply.github.com>
2018-02-01 19:46:50 +0300

    Update CUDA_wrappers.hpp

--- include/CUDA_wrappers.hpp
+++ include/CUDA_wrappers.hpp
@@ -1,1 +1,2 @@
-void compute_matrix(unsigned int* matrix, double x_down, double x_up, double y_down, double y_up, unsigned int width, unsigned int height, unsigned int iteration_count);
+#define PRECISION double
+void compute_matrix(unsigned int* matrix, PRECISION x_down, PRECISION x_up, PRECISION y_down, PRECISION y_up, unsigned int width, unsigned int height, unsigned int iteration_count);
